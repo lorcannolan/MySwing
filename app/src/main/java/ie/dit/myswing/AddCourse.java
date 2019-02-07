9b669c58784f933c9d038eb68b6d0afb1d956c4a
lorcannolan <c15513717@mydit.ie>
2019-02-07 18:14:44 +0000

    Google Places Auto Suggestion Box Working

--- app/src/main/java/ie/dit/myswing/AddCourse.java
+++ app/src/main/java/ie/dit/myswing/AddCourse.java
@@ -2,12 +2,66 @@ package ie.dit.myswing;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class AddCourse extends AppCompatActivity {
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+
+import java.util.Arrays;
+
+public class AddCourse extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap myMap;
+    private PlacesClient placesClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_course);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        getSupportActionBar().setTitle("Add New Course");
+
+        // Initialize Places, required as part of new static Places library rather than avilable through Play Servises
+        // https://developers.google.com/places/android-sdk/client-migration
+        if (!Places.isInitialized()) {
+            Places.initialize(getApplicationContext(), "AIzaSyBIo4_j-nnKuLtcqe4twifKoz-ZNcpO9gU");
+        }
+        // Create a new Places client instance.
+        placesClient = Places.createClient(this);
+        // Initialize the AutocompleteSupportFragment
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Toast.makeText(AddCourse.this, "Place: " + place.getName() + ", " + place.getId(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Toast.makeText(AddCourse.this, "An error occurred: " + status, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        myMap = googleMap;
+        myMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
     }
 }
