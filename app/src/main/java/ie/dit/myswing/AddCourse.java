74a3c23d666533222bb95d0976d32cc56b46c208
lorcannolan <c15513717@mydit.ie>
2019-02-08 16:09:50 +0000

    Courses can be added to database with extra validation incase course exists in database already

--- app/src/main/java/ie/dit/myswing/AddCourse.java
+++ app/src/main/java/ie/dit/myswing/AddCourse.java
@@ -1,7 +1,13 @@
 package ie.dit.myswing;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.common.api.Status;
@@ -15,21 +21,34 @@ import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Arrays;
+
+import ru.dimorinny.floatingtextbutton.FloatingTextButton;
 
 public class AddCourse extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap myMap;
     private PlacesClient placesClient;
+    private FloatingTextButton floatingTextButton;
+    private Place currentPlace;
+
+    private static final String TAG = "AddCourse";
+    private boolean dataPresent = false;
+
     DatabaseReference courseRef = FirebaseDatabase.getInstance().getReference().child("courses");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_course);
+
+        final CoordinatorLayout container = (CoordinatorLayout) findViewById(R.id.add_course_layout);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -46,8 +65,9 @@ public class AddCourse extends AppCompatActivity implements OnMapReadyCallback {
 
         // Initialize the AutocompleteSupportFragment
         // Info on autocomplete widget found here: https://developers.google.com/places/android-sdk/autocomplete
-        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+        final AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
                 getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+        autocompleteFragment.a.setHint("Search Course");
 
         // Data fields to be returned from selection are outlined here
         autocompleteFragment.setPlaceFields(Arrays.asList(
@@ -55,21 +75,63 @@ public class AddCourse extends AppCompatActivity implements OnMapReadyCallback {
                 Place.Field.NAME,
                 Place.Field.LAT_LNG,
                 Place.Field.ADDRESS,
-                Place.Field.RATING,
-                Place.Field.USER_RATINGS_TOTAL
+                Place.Field.WEBSITE_URI
         ));
 
         autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(Place place) {
-                // TODO: Get info about the selected place.
                 setCamera(place);
+                currentPlace = place;
+                dataPresent = false;
             }
 
             @Override
             public void onError(Status status) {
                 // TODO: Handle the error.
                 Toast.makeText(AddCourse.this, "An error occurred: " + status, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        floatingTextButton = (FloatingTextButton)findViewById(R.id.save);
+        floatingTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO:
+                //add location bias, hint, etc
+                if (autocompleteFragment.a.getText().toString().equals("")) {
+                    Snackbar.make(container, "Must Enter Text Value", Snackbar.LENGTH_LONG).show();
+                }
+                else {
+                    courseRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            for (DataSnapshot data : dataSnapshot.getChildren()) {
+                                // If data exists in database, break and set boolean
+                                if (data.child("placesID").getValue().toString().equals(currentPlace.getId())) {
+                                    dataPresent = true;
+                                    break;
+                                }
+                            }
+
+                            //If data doesn't exist, save course information
+                            if (!dataPresent) {
+                                String id = courseRef.push().getKey();
+                                courseRef.child(id).child("placesID").setValue(currentPlace.getId());
+                                courseRef.child(id).child("name").setValue(currentPlace.getName());
+                                courseRef.child(id).child("Address").setValue(currentPlace.getAddress());
+                                courseRef.child(id).child("location").setValue(currentPlace.getLatLng());
+                                courseRef.child(id).child("website").setValue(currentPlace.getWebsiteUri().toString());
+                                Snackbar.make(container, "Data Saved to Database", Snackbar.LENGTH_LONG).show();
+                            } else {
+                                Snackbar.make(container, "Data Already Exists in Database", Snackbar.LENGTH_LONG).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {}
+                    });
+                }
             }
         });
     }
