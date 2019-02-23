21616f315a5e8dd0c59ec911cbd761b17e4af5e8
lorcannolan <c15513717@mydit.ie>
2019-02-23 14:19:00 +0000

    Dropped pin saves to firebase, drag and drop of markers working and camera focusing on all markers of a hole functionality also added

--- app/src/main/java/ie/dit/myswing/ConfigureMapFragment.java
+++ app/src/main/java/ie/dit/myswing/ConfigureMapFragment.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -23,11 +24,17 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import ru.dimorinny.floatingtextbutton.FloatingTextButton;
 
@@ -38,9 +45,10 @@ public class ConfigureMapFragment extends Fragment implements OnMapReadyCallback
     private LatLng courseLatLng;
     private DatabaseReference holesRef;
 
-    private String courseFirebaseKey;
+    private String courseFirebaseKey, courseName;
     private Spinner selectHoleSpinner;
     private RelativeLayout spinnerContainer;
+    private String selectedHole;
 
     private static final String TAG = "ConfigureMapFragment";
 
@@ -55,7 +63,7 @@ public class ConfigureMapFragment extends Fragment implements OnMapReadyCallback
         Intent i = getActivity().getIntent();
         courseFirebaseKey = i.getStringExtra("courseFirebaseKey");
         String coursePlacesID = i.getStringExtra("coursePlacesID");
-        String courseName = i.getStringExtra("courseName");
+        courseName = i.getStringExtra("courseName");
         String courseLatitude = i.getStringExtra("courseLatitude");
         String courseLongitude = i.getStringExtra("courseLongitude");
 
@@ -71,15 +79,27 @@ public class ConfigureMapFragment extends Fragment implements OnMapReadyCallback
         selectHoleSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selectedHole = parent.getItemAtPosition(position).toString();
+                selectedHole = parent.getItemAtPosition(position).toString();
                 if (selectedHole.equalsIgnoreCase("-select hole-")) {
+                    myMap.clear();
                     myMap.moveCamera(CameraUpdateFactory.newLatLngZoom(courseLatLng, 15f));
                     MarkerOptions courseLocation = new MarkerOptions()
-                            .position(courseLatLng);
+                            .position(courseLatLng)
+                            .title(courseName)
+                            .draggable(true);
                     myMap.addMarker(courseLocation);
                 }
                 else {
                     myMap.clear();
+                    holesRef.child(selectedHole).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            displayMarkers(dataSnapshot);
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {}
+                    });
                 }
             }
 
@@ -95,16 +115,116 @@ public class ConfigureMapFragment extends Fragment implements OnMapReadyCallback
         return view;
     }
 
+    public void displayMarkers(DataSnapshot dataSnapshot) {
+        /*
+            Information relating to LatLngBounds was found here:
+                - https://stackoverflow.com/questions/15540220/google-map-camera-position-on-multiple-markers
+            LatLngBounds enables camera to zoom to markers and display all markers within the bounds of the screen.
+         */
+        LatLngBounds.Builder mapBoundsBuilder = new LatLngBounds.Builder();
+        boolean locationsToShow = false;
+        // Men's Tee Box Marker
+        LatLng mensTeeBox = new LatLng(
+                Double.parseDouble(dataSnapshot.child("mens tee box").child("latitude").getValue().toString()),
+                Double.parseDouble(dataSnapshot.child("mens tee box").child("longitude").getValue().toString())
+        );
+        if (!mensTeeBox.equals(courseLatLng)) {
+            mapBoundsBuilder.include(mensTeeBox);
+            locationsToShow = true;
+            MarkerOptions mensTeeBoxMarker = new MarkerOptions()
+                    .position(mensTeeBox)
+                    .title(selectedHole + ". Men's Tee Box")
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                    .draggable(true);
+            myMap.addMarker(mensTeeBoxMarker);
+        }
+
+        // Ladies Tee Box Marker
+        LatLng ladiesTeeBox = new LatLng(
+                Double.parseDouble(dataSnapshot.child("ladies tee box").child("latitude").getValue().toString()),
+                Double.parseDouble(dataSnapshot.child("ladies tee box").child("longitude").getValue().toString())
+        );
+        if (!ladiesTeeBox.equals(courseLatLng)) {
+            mapBoundsBuilder.include(ladiesTeeBox);
+            locationsToShow = true;
+            MarkerOptions ladiesTeeBoxMarker = new MarkerOptions()
+                    .position(ladiesTeeBox)
+                    .title(selectedHole + ". Ladies Tee Box")
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE))
+                    .draggable(true);
+            myMap.addMarker(ladiesTeeBoxMarker);
+        }
+
+        // Front of Green Marker
+        LatLng frontOfGreen = new LatLng(
+                Double.parseDouble(dataSnapshot.child("front green").child("latitude").getValue().toString()),
+                Double.parseDouble(dataSnapshot.child("front green").child("longitude").getValue().toString())
+        );
+        if (!frontOfGreen.equals(courseLatLng)) {
+            mapBoundsBuilder.include(frontOfGreen);
+            locationsToShow = true;
+            MarkerOptions frontOfGreenMarker = new MarkerOptions()
+                    .position(frontOfGreen)
+                    .title(selectedHole + ". Front of Green")
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+                    .draggable(true);
+            myMap.addMarker(frontOfGreenMarker);
+        }
+
+        // Middle of Green Marker
+        LatLng middleOfGreen = new LatLng(
+                Double.parseDouble(dataSnapshot.child("middle green").child("latitude").getValue().toString()),
+                Double.parseDouble(dataSnapshot.child("middle green").child("longitude").getValue().toString())
+        );
+        if (!middleOfGreen.equals(courseLatLng)) {
+            mapBoundsBuilder.include(middleOfGreen);
+            locationsToShow = true;
+            MarkerOptions middleOfGreenMarker = new MarkerOptions()
+                    .position(middleOfGreen)
+                    .title(selectedHole + ". Middle of Green")
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW))
+                    .draggable(true);
+            myMap.addMarker(middleOfGreenMarker);
+        }
+
+        // Back of Green Marker
+        LatLng backOfGreen = new LatLng(
+                Double.parseDouble(dataSnapshot.child("back green").child("latitude").getValue().toString()),
+                Double.parseDouble(dataSnapshot.child("back green").child("longitude").getValue().toString())
+        );
+        if (!backOfGreen.equals(courseLatLng)) {
+            mapBoundsBuilder.include(backOfGreen);
+            locationsToShow = true;
+            MarkerOptions backOfGreenMarker = new MarkerOptions()
+                    .position(backOfGreen)
+                    .title(selectedHole + ". Back of Green")
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+                    .draggable(true);
+            myMap.addMarker(backOfGreenMarker);
+        }
+
+        if (!locationsToShow) {
+            myMap.moveCamera(CameraUpdateFactory.newLatLngZoom(courseLatLng, 15f));
+            MarkerOptions courseLocation = new MarkerOptions()
+                    .position(courseLatLng)
+                    .title(courseName)
+                    .draggable(true);
+            myMap.addMarker(courseLocation);
+            Snackbar.make(getView(), "No saved locations for hole " + selectedHole, Snackbar.LENGTH_LONG).show();
+        }
+        else {
+            LatLngBounds mapBounds = mapBoundsBuilder.build();
+            myMap.moveCamera(CameraUpdateFactory.newLatLngBounds(mapBounds, 100));
+        }
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         myMap = googleMap;
         myMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
         myMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
             @Override
-            public void onMapLongClick(LatLng latLng) {
-                MarkerOptions droppedMarker = new MarkerOptions()
-                        .position(latLng);
-                myMap.addMarker(droppedMarker);
+            public void onMapLongClick(final LatLng latLng) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
                 builder.setTitle("Select the Location Type for this Marker.");
                 builder.setItems(R.array.location_type, new DialogInterface.OnClickListener() {
@@ -112,8 +232,38 @@ public class ConfigureMapFragment extends Fragment implements OnMapReadyCallback
                     public void onClick(DialogInterface dialog, int which) {
                         Resources res = getContext().getResources();
                         String[] locationType = res.getStringArray(R.array.location_type);
-                        // Sets the text of the edit text widget that was clicked by using an ArrayList to store all of the individual edit texts
                         String chosenLocationType = (locationType[which]);
+                        String firebasePath = "";
+                        if (!selectedHole.equalsIgnoreCase("-select hole-")) {
+                            switch (chosenLocationType) {
+                                case "Men's Tee Box":
+                                    firebasePath = "mens tee box";
+                                    break;
+                                case "Ladies Tee Box":
+                                    firebasePath = "ladies tee box";
+                                    break;
+                                case "Front of Green":
+                                    firebasePath = "front green";
+                                    break;
+                                case "Middle of Green":
+                                    firebasePath = "middle green";
+                                    break;
+                                case "Back of Green":
+                                    firebasePath = "back green";
+                                    break;
+                            }
+                            holesRef.child(selectedHole).child(firebasePath).setValue(latLng);
+                            myMap.clear();
+                            holesRef.child(selectedHole).addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                    displayMarkers(dataSnapshot);
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) {}
+                            });
+                        }
                     }
                 });
                 builder.setNegativeButton("Cancel",
@@ -125,6 +275,39 @@ public class ConfigureMapFragment extends Fragment implements OnMapReadyCallback
 
                 AlertDialog chooselocationType = builder.create();
                 chooselocationType.show();
+            }
+        });
+
+        myMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDragStart(Marker marker) {
+            }
+
+            @Override
+            public void onMarkerDrag(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+                String firebasePath = "";
+                if (marker.getTitle().contains("Men's")) {
+                    firebasePath = "mens tee box";
+                }
+                else if (marker.getTitle().contains("Ladies")) {
+                    firebasePath = "ladies tee box";
+                }
+                else if (marker.getTitle().contains("Front")) {
+                    firebasePath = "front green";
+                }
+                else if (marker.getTitle().contains("Middle")) {
+                    firebasePath = "middle green";
+                }
+                else if (marker.getTitle().contains("Back")) {
+                    firebasePath = "back green";
+                }
+                holesRef.child(selectedHole).child(firebasePath).child("latitude").setValue(marker.getPosition().latitude);
+                holesRef.child(selectedHole).child(firebasePath).child("longitude").setValue(marker.getPosition().longitude);
             }
         });
     }
